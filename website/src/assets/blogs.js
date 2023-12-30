41efecca4f4eb2597658d1f39aa395081a12f869
Matthew Vandenberg <mv5903@gmail.com>
2023-12-30 10:43:04 -0500

    redorder blogs

--- website/src/assets/blogs.js
+++ website/src/assets/blogs.js
@@ -1,10 +1,10 @@
 export const blogs = [
-    // {
-    //     "title": "Running Windows 11 and macOS Sonoma Simultaneously: How I Did it",
-    //     "date": "December 10, 2023",
-    //     "description": "I have been running Windows 11 and macOS Sonoma on my PC, here's how I did it.",
-    //     "blog": "win11-macos-sonoma"
-    // },
+    {
+        "title": "Simulataneously booting Windows 11 and MacOS Sonoma",
+        "date": "December 30, 2023",
+        "description": "You can get near native performance on both Windows 11 and MacOS Sonoma running on the same machine with Proxmox. See why I eventually opted out.",
+        "blog": "win11-macos-sonoma"
+    },
     {
         "title": "Jumbotron: A New Way to Display Your Favorite Images",
         "date": "December 7, 2023",
@@ -16,11 +16,5 @@ export const blogs = [
         "date": "November 10, 2023",
         "description": "Partyfy is a web app that allows you to queue music to your friend's Spotify account without needing their session, through a system I call \"Unattended Queuing\". See what's involved.",
         "blog": "partyfy"
-    },
-    {
-        "title": "Simulataneously booting Windows 11 and MacOS Sonoma",
-        "date": "December 30, 2023",
-        "description": "You can get near native performance on both Windows 11 and MacOS Sonoma running on the same machine with Proxmox. See why I eventually opted out.",
-        "blog": "win11-macos-sonoma"
     }
 ]
