166465b9284cdc105410a3a23885f24c40822d12
Matthew Vandenberg <mv5903@gmail.com>
2024-07-03 17:17:54 -0700

    fix light mode bug with blogs

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -206,14 +206,14 @@
     <section id="blog" class="px-4 py-8 text-left">
       <h3 class="text-3xl font-bold text-center">Blog</h3>
       <div class="flex justify-center mt-4">
-        <ol class="relative border-s border-gray-200 dark:border-gray-700">
+        <ol class="relative border-s border-gray-700">
           {#each blogs as blog}
           <li class="mb-10 ms-4">
-            <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{blog.date}</time>
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{blog.title}</h3>
-            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{blog.description}</p>
-            <a href={`http://home.mattvandenberg.com:3000/blog/${blog.blog}`} target="_blank" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Read <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <div class="absolute w-3 h-3rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+            <time class="mb-1 text-sm font-normal leading-none text-gray-500">{blog.date}</time>
+            <h3 class="text-lg font-semibold text-white">{blog.title}</h3>
+            <p class="mb-4 text-base font-normal text-gray-400">{blog.description}</p>
+            <a href={`http://home.mattvandenberg.com:3000/blog/${blog.blog}`} target="_blank" class="inline-flex items-center px-4 py-2 text-sm font-medium  border  rounded-lg focus:z-10 focus:ring-4 focus:outline-none bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700">Read <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg></a>
           </li>
