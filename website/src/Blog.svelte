ba586cb218156f50fcbbbeb90959d71558daac93
Matthew Vandenberg <mv5903@gmail.com>
2024-10-28 13:44:20 -0400

    fix blog link

--- website/src/Blog.svelte
+++ website/src/Blog.svelte
@@ -11,7 +11,7 @@
         if (!blog) {
             backToHome();
         }
-        fetch(`http://localhost:3000/blogs`)
+        fetch(`https://mattvandenberg.com:3000/blogs`)
             .then((res) => res.json())
             .then((data) => {
                 currentBlogHtml = data.find((b) => b.name === blog.blog).content;
